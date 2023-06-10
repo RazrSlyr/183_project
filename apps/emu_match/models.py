9aeaee291835bf7c3fa173867be98ecf5a7a78de
SteveX9527 <xxie36@ucsc.edu>
2023-06-09 22:37:30 -0700

    change

--- apps/emu_match/models.py
+++ apps/emu_match/models.py
@@ -18,25 +18,17 @@ def get_time():
     return datetime.datetime.utcnow()
 
 
-# Define your table below
-#
-# db.define_table('thing', Field('name'))
-#
-# always commit your models to avoid problems later
 
-# RV: All games a player can queue for
 db.define_table("games",
                 Field("name"),
                 Field("platform"))
 
-# RV: All matched lobbies (or lobbies in the process of being matched)
 db.define_table("lobbies",
                 Field("user_1", "reference auth_user", default=get_user_id),
                 Field("user_2", "reference auth_user"),
                 Field("game", "reference games"))
 
 
-# MP: All chats seperated by lobby_num
 db.define_table("chat",
                 Field("lobby_num"),
                 Field("user", "reference auth_user"),
